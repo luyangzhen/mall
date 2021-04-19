3c223a77196423a018a67730e36a7db9057155cb
luyangzhen <1127820919@qq.com>
2021-04-19 11:52:23 +0800

    vue提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="wrapper">
+    <keep-alive exclude="Detail">
+      <router-view></router-view>
+    </keep-alive>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+import MainTabBar from 'components/content/MainTabBar/MainTabBar'
+export default {
+  name: 'App',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
+<style scoped>
+@import "./assets/css/base.css";
+</style>
